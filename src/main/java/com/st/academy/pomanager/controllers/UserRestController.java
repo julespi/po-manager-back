580f4d288d5a2eb4d1d99b262a527d421d730259
Julián Nicolás Spinelli <julian.spinelli.so@softtekosde.com>
2022-01-27 14:19:10 -0300

    agregado log para rastrar bug en detalles

--- src/main/java/com/st/academy/pomanager/controllers/UserRestController.java
+++ src/main/java/com/st/academy/pomanager/controllers/UserRestController.java
@@ -92,6 +92,7 @@ public class UserRestController implements CrudRestController<UserDTO> {
                 .stream()
                 .map(order -> modelMapper.map(order, OrderDTO.class))
                 .collect(Collectors.toList());
+        System.out.println(ordersDTO.size());
         response.put("payload", ordersDTO);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
